7c59a14d1002433b1d9b99fa33571a4bb49cb100
Jens <jr@jensravn.com>
2020-10-30 17:14:10 +0100

    max() walk through. Fix recursive call + added tests

--- week1/example/src/main/scala/example/Lists.scala
+++ week1/example/src/main/scala/example/Lists.scala
@@ -7,25 +7,25 @@ import scala.annotation.tailrec
 object Lists {
 
   /**
-   * This method computes the sum of all elements in the list xs. There are
-   * multiple techniques that can be used for implementing this method, and
-   * you will learn during the class.
-   *
-   * For this example assignment you can use the following methods in class
-   * `List`:
-   *
-   *  - `xs.isEmpty: Boolean` returns `true` if the list `xs` is empty
-   *  - `xs.head: Int` returns the head element of the list `xs`. If the list
-   *    is empty an exception is thrown
-   *  - `xs.tail: List[Int]` returns the tail of the list `xs`, i.e. the the
-   *    list `xs` without its `head` element
-   *
-   *  ''Hint:'' instead of writing a `for` or `while` loop, think of a recursive
-   *  solution.
-   *
-   * @param xs A list of natural numbers
-   * @return The sum of all elements in `xs`
-   */
+    * This method computes the sum of all elements in the list xs. There are
+    * multiple techniques that can be used for implementing this method, and
+    * you will learn during the class.
+    *
+    * For this example assignment you can use the following methods in class
+    * `List`:
+    *
+    *  - `xs.isEmpty: Boolean` returns `true` if the list `xs` is empty
+    *  - `xs.head: Int` returns the head element of the list `xs`. If the list
+    *    is empty an exception is thrown
+    *  - `xs.tail: List[Int]` returns the tail of the list `xs`, i.e. the the
+    *    list `xs` without its `head` element
+    *
+    * ''Hint:'' instead of writing a `for` or `while` loop, think of a recursive
+    * solution.
+    *
+    * @param xs A list of natural numbers
+    * @return The sum of all elements in `xs`
+    */
   def sum(xs: List[Int]): Int = {
     /*
     // Pattern matching solution
@@ -43,18 +43,18 @@ object Lists {
   }
 
   /**
-   * This method returns the largest element in a list of integers. If the
-   * list `xs` is empty it throws a `java.util.NoSuchElementException`.
-   *
-   * You can use the same methods of the class `List` as mentioned above.
-   *
-   * ''Hint:'' Again, think of a recursive solution instead of using looping
-   * constructs. You might need to define an auxiliary method.
-   *
-   * @param xs A list of natural numbers
-   * @return The largest element in `xs`
-   * @throws java.util.NoSuchElementException if `xs` is an empty list
-   */
+    * This method returns the largest element in a list of integers. If the
+    * list `xs` is empty it throws a `java.util.NoSuchElementException`.
+    *
+    * You can use the same methods of the class `List` as mentioned above.
+    *
+    * ''Hint:'' Again, think of a recursive solution instead of using looping
+    * constructs. You might need to define an auxiliary method.
+    *
+    * @param xs A list of natural numbers
+    * @return The largest element in `xs`
+    * @throws java.util.NoSuchElementException if `xs` is an empty list
+    */
   def max(xs: List[Int]): Int = {
     if (xs.isEmpty) throw new NoSuchElementException()
     maxRecursive(Int.MinValue, xs)
@@ -62,7 +62,7 @@ object Lists {
 
   @tailrec
   private def maxRecursive(max: Int, xs: List[Int]): Int = {
-    if(xs.tail.isEmpty)
+    if (xs.isEmpty)
       return max
     val newMax = if (xs.head > max) xs.head else max
     maxRecursive(newMax, xs.tail)
